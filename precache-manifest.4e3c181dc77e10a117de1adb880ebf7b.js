27043fc2ad94c04b315841a202f8b12c5aa08b09
Kenny Hefner <khefner@wbros.com>
2020-09-08 17:15:55 -0400

    Updates

---
+++ precache-manifest.4e3c181dc77e10a117de1adb880ebf7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0c3ff70b1ad22d072947f09d0aad32",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "f6631128792ebe562a82",
+    "url": "/react-tutorial/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6631128792ebe562a82",
+    "url": "/react-tutorial/static/js/main.629ef3db.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
